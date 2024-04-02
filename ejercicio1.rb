28f2648b01e030152f98314c29991f98fd3efc2d
JocRivera <jose_mrivera@soy.sena.edu.co>
2024-04-02 09:22:01 -0500

    ejercicios

---
+++ ejercicio1.rb
@@ -0,0 +1,10 @@
+puts "Hola mundo"
+#ejercicio1
+#pedir dos numeros y realizar una suma
+puts"ingrese el numero 1"
+numeroUno= gets.to_i
+puts"ingrese el numero 2"
+numeroDos= gets.to_i
+total= numeroDos+numeroUno
+# puts "total es igual a ", total
+puts "total es igual a #{total} "
