42a4bf4d57d9288c375194725781405605f44854
JocRivera <jose_mrivera@soy.sena.edu.co>
2024-04-02 09:35:36 -0500

    funcion

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,1 +1,9 @@
-
+def validarElMayor(numeroUno, numeroDos)
+  if numeroUno > numeroDos
+    return numeroUno
+  else
+    return numeroDos
+  end
+end
+print validarElMayor(3. 6)
+print validarElMayor(6, 3)
